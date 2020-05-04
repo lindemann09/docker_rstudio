ce7e060c9e3ba69440a6e0f73bfdf67890a7bfde
Oliver Lindemann <lindemann09@users.noreply.github.com>
2020-05-04 21:22:57 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install docker-compose
+
+sleep 3
+
 sudo gpasswd -a $USER docker
-
-sleep 5
-
 echo "PLEASE LOG OUT AND IN AGAIN. RUN: docker-compose up -d"
